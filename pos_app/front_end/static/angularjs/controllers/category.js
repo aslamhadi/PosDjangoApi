d7776b1d56cba83e35078a9dd944b478bdc47419
aslam <aslamhadi@gmail.com>
2014-10-04 00:50:49 +0700

    Refactor category controller

--- pos_app/front_end/static/angularjs/controllers/category.js
+++ pos_app/front_end/static/angularjs/controllers/category.js
@@ -2,30 +2,27 @@ posControllers.controller('CategoryCtrl', ['$scope', 'categoryService',
     function ($scope, categoryService) {
         $scope.categories = [];
 
-        loadRemoteData();
+        getCategories();
 
-        function loadRemoteData() {
-
+        function getCategories() {
             // The categoryService returns a promise.
             categoryService.getCategories()
                 .then(
                 function (categories) {
                     $scope.categories = categories;
                 }
-            )
-            ;
-
+            );
         }
 
         $scope.addCategory = function () {
-            // If the data we provide is invalid, the promise will be rejected,
-            // at which point we can tell the user that something went wrong. In
-            // this case, I'm just logging to the console to keep things very
-            // simple for the demo.
             categoryService.addCategory($scope.form.name)
                 .then(
-                loadRemoteData,
+                function (category) {
+                    // The api return data so let's just push the data into categories array
+                    $scope.categories.push(category);
+                },
                 function (errorMessage) {
+                    // Add warning if something unpleasant happens
                     console.warn(errorMessage);
                 }
             );
