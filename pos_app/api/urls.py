b80150dc415fb8102283cefc73f0b05c1c5e279a
aslam <aslamhadi@gmail.com>
2014-10-04 10:02:20 +0700

    Create api to update and delete category

--- pos_app/api/urls.py
+++ pos_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from pos_app.api.views import UserListCreate, CategoryListCreate
+from pos_app.api.views import UserListCreate, CategoryListCreate, CategoryDetail
 
 urlpatterns = patterns('',  # nopep8
     # User
     url(r'^users/$', UserListCreate.as_view(), name='user'),
     url(r'^categories/$', CategoryListCreate.as_view(), name='category'),
+    url(r'^categories/(?P<pk>[0-9]+)/$', CategoryDetail.as_view(), name='category-detail'),
 )
--- pos_app/api/views.py
+++ pos_app/api/views.py
@@ -13,3 +13,8 @@ class UserListCreate(ListCreateAPIView):
 class CategoryListCreate(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
