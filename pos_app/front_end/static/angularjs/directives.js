c4a175cb1cc62fe2395807ad996bc78aca9827e1
aslam <aslamhadi@gmail.com>
2014-12-24 17:33:30 +0700

    Add numeric directive

---
+++ pos_app/front_end/static/angularjs/directives.js
@@ -0,0 +1,18 @@
+angular.module('posAngular')
+.directive('numeric', function() {
+    return {
+      require: 'ngModel',
+      link: function (scope, element, attr, ngModelCtrl) {
+        function fromUser(text) {
+          var transformedInput = text.replace(/[^0-9]/g, '');
+          if(transformedInput !== text) {
+              ngModelCtrl.$setViewValue(transformedInput);
+              ngModelCtrl.$render();
+          }
+          return transformedInput;  // or return Number(transformedInput)
+        }
+        ngModelCtrl.$parsers.push(fromUser);
+      }
+    }
+});
+
